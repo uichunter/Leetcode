fbd92b383763d187c1f8d02f4ac9e25c4170c845
uichunter <pankettingpanda@gmail.com>
2016-09-17 23:34:42 +0800

    Add number 1.

---
+++ Number1_TwoSum/Number1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode
+{
+    class Number1
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            //    for (int i = 0; i < nums.Length - 1; i++)
+            //    {
+            //        for (int j = i + 1; j < nums.Length; j++)
+            //        {
+            //            if (nums[i] + nums[j] == target)
+            //                return new int[] { i,j};
+            //        }
+            //    }
+            //    return new int[0];
+            //
+            int j;
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int com = target - nums[i];
+                if (dic.TryGetValue(com, out j))
+                {
+                    int[] result = new int[] { j, i };
+                    return result;
+                }
+                dic[com] = i;
+            }
+            return new int[0];
+        }
+    }
+        
+}
